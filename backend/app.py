fec147c2afdb11b85424f7d6308102a2b9e2f169
DevKadiya7 <kadiyadev07@gmail.com>
2025-06-19 12:49:51 +0530

    Save my changes to app.py

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from fetch_news import get_news
+
+app = Flask(__name__)
+
+@app.route("/api/disasters")
+def get_disasters():
+    news_data = get_news()
+    return jsonify(news_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
